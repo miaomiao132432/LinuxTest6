11daa9b9c73f0403a1d9e770db9fb521dd53a874
miaomiaoda <ueny.nx@gmail.com>
2021-11-24 11:27:26 +0800

    update

--- Text2.java
+++ Text2.java
@@ -8,15 +8,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@WebServlet(urlPatterns = "/Text1")
-public class Text1 extends HttpServlet {
+@WebServlet(urlPatterns = "/Text2")
+public class Text2 extends HttpServlet {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://116.62.123.4/linux_final";
     static final String USER = "miaomiao";
     static final String PASS = "";
 
    static Connection conn = null;
-
+   static String name = null;
    public void init() {
       try {
          Class.forName(JDBC_DRIVER);
@@ -37,11 +37,13 @@ public class Text1 extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
           response.setContentType("text/html");
           PrintWriter out = response.getWriter();
-
-          Student stu = getStudent();
-
-
-          out.println("<h1>hello world, " + stu.name + "</h1>");
+          if (name == null) {
+            Student stu = getStudent();
+            name = stu.name;
+             out.println("<h1>hello world, " + stu.name + "</h1>");
+          } else {
+             out.println("<h1>hello world, " + name + "</h1>");
+           }
     }
     
     public Student getStudent() {
--- Text3.java
+++ Text3.java
@@ -7,20 +7,23 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import redis.clients.jedis.Jedis;
 
-@WebServlet(urlPatterns = "/Text1")
-public class Text1 extends HttpServlet {
+@WebServlet(urlPatterns = "/Text3")
+public class Text3 extends HttpServlet {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://116.62.123.4/linux_final";
     static final String USER = "miaomiao";
     static final String PASS = "";
-
+ 
    static Connection conn = null;
-
+   static Jedis jedis = null;
    public void init() {
       try {
          Class.forName(JDBC_DRIVER);
          conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         jedis = new Jedis("116.62.123.4"); 
+
       } catch (Exception e) {
          e.printStackTrace();
       }
@@ -37,11 +40,16 @@ public class Text1 extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
           response.setContentType("text/html");
           PrintWriter out = response.getWriter();
-
-          Student stu = getStudent();
-
-
-          out.println("<h1>hello world, " + stu.name + "</h1>");
+          String name = jedis.get("name"); 
+     
+          if (name == null) {
+            Student stu = getStudent();
+            jedis.set("name", stu.name);
+ 
+            out.println("<h1>hello world, " + stu.name + "</h1>");
+          } else {
+             out.println("<h1>hello world, " + name + "</h1>");
+           }
     }
     
     public Student getStudent() {
